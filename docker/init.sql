91a5a3250e2a19c69aab86cb20e41f03e18f6b2e
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-01-29 11:48:08 -0300

    project setup: integrate zod and add docker compose

---
+++ docker/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE upload_test;
+
+-- When the docker goes up, it will run this script to create the database
+-- And will run whatever other sql file that is in the folder
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,40 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
+
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+// Error handling global for the application - All errors that are not handled by the route itself (validation included)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: error.validation,
+    })
+  }
+
+  // Send the error for a observability tool (Sentry/Datadog/Grafana/...)
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
+
+// CORS Configuration
 server.register(fastifyCors, { origin: '*' })
+
+// Routes Configuration
+server.register(uploadImageRoute)
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('🚀 HTTP Server is running')
 })
