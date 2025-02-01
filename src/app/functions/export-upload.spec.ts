b45b59af645289edf1797c575b5ee7d386a5fdd8
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-02-01 14:20:43 -0300

    test: export uploads

--- src/app/functions/export-upload.spec.ts
+++ src/app/functions/export-upload.spec.ts
@@ -1,10 +1,24 @@
 import { randomUUID } from 'node:crypto'
+import * as upload from '@/infra/storage/upload-file-to-storage'
+import { isRight, unwrapEither } from '@/shared/either'
 import { makeUpload } from '@/test/factories/make-upload'
-import { describe, it } from 'vitest'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
 import { exportUploads } from './export-uploads'
 
 describe('export uploads', () => {
+  /**
+   * ensure that the csv content was generated correctly
+   */
   it('should be able to export uploads', async () => {
+    const uploadStub = vi
+      .spyOn(upload, 'uploadFileToStorage')
+      .mockImplementationOnce(async () => {
+        return {
+          key: `${randomUUID()}.csv`,
+          url: 'http://example.com/file.csv',
+        }
+      })
+
     const namePattern = randomUUID()
 
     const upload1 = await makeUpload({ name: `${namePattern}.webp` })
@@ -16,5 +30,44 @@ describe('export uploads', () => {
     const sut = await exportUploads({
       searchQuery: namePattern,
     })
+
+    const generatedCSVStream = uploadStub.mock.calls[0][0].contentStream // [0][0] First time that the function was called and first parameter
+
+    const csvAsString = await new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = []
+
+      generatedCSVStream.on('data', (chunk: Buffer) => {
+        chunks.push(chunk)
+      })
+
+      generatedCSVStream.on('end', () => {
+        resolve(Buffer.concat(chunks).toString('utf-8'))
+      })
+
+      generatedCSVStream.on('error', err => {
+        reject(err)
+      })
+    })
+
+    console.log(csvAsString)
+    const csvAsArray = csvAsString
+      .trim()
+      .split('\n')
+      .map(row => row.split(','))
+
+    console.log(csvAsArray)
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut)).toEqual({
+      reportUrl: 'http://example.com/file.csv',
+    })
+    expect(csvAsArray).toEqual([
+      ['ID', 'Name', 'Remote URL', 'Created At'],
+      [upload1.id, upload1.name, upload1.remoteUrl, expect.any(String)],
+      [upload2.id, upload2.name, upload2.remoteUrl, expect.any(String)],
+      [upload3.id, upload3.name, upload3.remoteUrl, expect.any(String)],
+      [upload4.id, upload4.name, upload4.remoteUrl, expect.any(String)],
+      [upload5.id, upload5.name, upload5.remoteUrl, expect.any(String)],
+    ])
   })
 })
